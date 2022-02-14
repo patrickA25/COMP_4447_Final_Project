84b7af514c0f57e29f905572f78d3815ffda3829
patrickA25 <patrickayers25@gmail.com>
2022-02-13 16:49:38 -0800

    removing not needed files

--- Getting_coin_data_test.py
+++ Getting_coin_data_test.py
@@ -1,8 +1,7 @@
 import pycoingecko as pycoin
 import pandas as pd
 import numpy as np
-import top_5_data as top
-import CointTableCode as ctc
+import Coin_Table_Code as ctc
 import testing_connection as tc
 from rich.console import Console
 from rich.table import Table
@@ -11,7 +10,7 @@ from rich.table import Table
 def main():
     cg = pycoin.CoinGeckoAPI()
     print(tc.testing_coin_connection(cg))
-    top.make_top_5_table(cg,['bitcoin', 'tether', 'ethereum','solana','binance coin'])
+    ctc.make_top_5_table(cg,['bitcoin', 'tether', 'ethereum','solana','binance coin'])
     ctc.top_7_table(cg)
 
 if __name__ == '__main__':
